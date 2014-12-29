83349e0403d1fc80d8f3f76f440513aa97e0c2ef
Silent <zdanio95@gmail.com>
2014-12-29 14:28:55 +0100

    Fixed indents

--- Engine/CCustomCarEnvMapPipeline.cpp
+++ Engine/CCustomCarEnvMapPipeline.cpp
@@ -225,7 +225,7 @@ void CCustomCarEnvMapPipeline::CustomPipeRenderCB(RwResEntry* repEntry, void* ob
     RwBool lighting = 0;
     RwD3D9GetRenderState(D3DRS_LIGHTING, &lighting);
     RwBool isLit;
-	RwBool renderDark = RpLightGetFlags(pDirect) & rpLIGHTLIGHTATOMICS && CVisibilityPlugins::GetAtomicId(atomic) & 0x4000;	// R* added this in 1.01
+    RwBool renderDark = RpLightGetFlags(pDirect) & rpLIGHTLIGHTATOMICS && CVisibilityPlugins::GetAtomicId(atomic) & 0x4000; // R* added this in 1.01
     if(lighting || flags & rxGEOMETRY_PRELIT)
     {
         isLit = 0;
@@ -276,7 +276,7 @@ void CCustomCarEnvMapPipeline::CustomPipeRenderCB(RwResEntry* repEntry, void* ob
         {
             applySpecMap = *materialFlags & 4;
         }
-        if(applySpecMap && !renderDark)	// R* added renderDark in 1.01)
+        if(applySpecMap && !renderDark) // R* added renderDark in 1.01)
         {
             CustomSpecMapPipeMaterialData* specMapData = PLUGIN_SPECMAP(material, data);
             RwD3D9SetLight(1, &gCarEnvMapLight);
@@ -294,14 +294,14 @@ void CCustomCarEnvMapPipeline::CustomPipeRenderCB(RwResEntry* repEntry, void* ob
         }
         RwD3D9SetTextureStageState(1, D3DTSS_COLOROP, D3DTOP_DISABLE);
         RwD3D9SetTextureStageState(2, D3DTSS_COLOROP, D3DTOP_DISABLE);
-        if(applyEnvMap && !renderDark)	// R* added renderDark in 1.01
+        if(applyEnvMap && !renderDark)  // R* added renderDark in 1.01
         {
             CustomEnvMapPipeMaterialData* envMapData = PLUGIN_ENVMAP(material, data);
             D3DMATRIX EnvMapTransform;
             RwRenderStateSet(rwRENDERSTATETEXTUREADDRESS, (void*)1);
-			// As the matrix is allocated on stack (unlike in R*'s code), the matrix should be zeroed first
-			// so untouched fields don't contain garbage
-			memset(&EnvMapTransform, 0, sizeof(EnvMapTransform));
+            // As the matrix is allocated on stack (unlike in R*'s code), the matrix should be zeroed first
+            // so untouched fields don't contain garbage
+            memset(&EnvMapTransform, 0, sizeof(EnvMapTransform));
             EnvMapTransform._11 = FixedPointToFloat<8>(envMapData->scaleX);
             EnvMapTransform._22 = FixedPointToFloat<8>(envMapData->scaleY);
             EnvMapTransform._33 = 1.0f;
@@ -326,16 +326,16 @@ void CCustomCarEnvMapPipeline::CustomPipeRenderCB(RwResEntry* repEntry, void* ob
             RwD3D9SetTextureStageState(1, D3DTSS_TEXTURETRANSFORMFLAGS, D3DTTFF_PROJECTED | D3DTTFF_COUNT1 | D3DTTFF_COUNT2);
             RwD3D9SetTextureStageState(2, D3DTSS_COLOROP, D3DTOP_DISABLE);
         }
-        if(applyAlphaBlending && !renderDark)	// R* added renderDark in 1.01)
+        if(applyAlphaBlending && !renderDark)   // R* added renderDark in 1.01)
         {
             CustomEnvMapPipeAtomicData* envMapAtmData = AllocEnvMapPipeAtomicData(atomic);
             CustomEnvMapPipeMaterialData* envMapData = PLUGIN_ENVMAP(material, data);
             D3DMATRIX AlphaBlendTransform;
             RwV2d scale;
             D3D9GetVectorForAlphaBlendingTransform(atomic, envMapAtmData, envMapData, &scale);
-			// As the matrix is allocated on stack (unlike in R*'s code), the matrix should be zeroed first
-			// so untouched fields don't contain garbage
-			memset(&AlphaBlendTransform, 0, sizeof(AlphaBlendTransform));
+            // As the matrix is allocated on stack (unlike in R*'s code), the matrix should be zeroed first
+            // so untouched fields don't contain garbage
+            memset(&AlphaBlendTransform, 0, sizeof(AlphaBlendTransform));
             AlphaBlendTransform._11 = 1.0f;
             AlphaBlendTransform._22 = 1.0f;
             AlphaBlendTransform._33 = 1.0f;
